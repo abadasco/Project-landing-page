fa71fe44f2c80dbced1a202520021c4752f0a54a
abadasco <arianbadasco@gmail.com>
2024-05-17 23:30:40 +0300

    commit small changes

--- Java script/script.js
+++ Java script/script.js
@@ -2,7 +2,6 @@ const switchModeButton = document.getElementById('switchMode');
 const themeLink = document.getElementById('theme');
 
 switchModeButton.addEventListener('click', () => {
-    // Проверяем текущий режим и переключаемся на противоположный
     if (themeLink.getAttribute('href') === '/CSS/light-mode.css') {
         themeLink.setAttribute('href', '/CSS/dark-mode.css');
     } else {
