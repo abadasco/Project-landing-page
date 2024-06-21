321fdf0ad8fe36bd4412dabcb4c95661de0c4716
abadasco <arianbadasco@gmail.com>
2024-06-21 17:28:26 +0300

    commit

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,6 @@
 //Light-Dark Mode
 const switchModeButton = document.getElementById('switchMode');
 const themeLink = document.getElementById('theme');
-
 switchModeButton.addEventListener('click', () => {
     if (themeLink.getAttribute('href') === '/CSS/light-mode.css') {
         themeLink.setAttribute('href', '/CSS/dark-mode.css');
@@ -9,18 +8,15 @@ switchModeButton.addEventListener('click', () => {
         themeLink.setAttribute('href', '/CSS/light-mode.css');
     }
 });
-
 //Sidebar
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.querySelector(".main").style.marginLeft = "250px";
 }
-
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.querySelector(".main").style.marginLeft= "0";
 }
-
 //Timpul din main
 function updateClock() {
     const clock = document.getElementById('clock');
@@ -30,30 +26,19 @@ function updateClock() {
     const seconds = String(now.getSeconds()).padStart(2, '0');
     clock.textContent = `${hours}:${minutes}:${seconds}`;
 }
-
 setInterval(updateClock, 1000);
 updateClock();  
-
 // Eventul si imaginea
 var button = document.getElementById("showImageButton");
 var imageContainer = document.getElementById("imageContainer");
-
-
 button.addEventListener("click", function() {
-
   var img = document.createElement("img");
   img.src = "/Foto/not_available.jpg"; 
   img.alt = "Imagine de exemplu";
   img.id = "exampleImage";
-
-  
   imageContainer.appendChild(img);
-
-
   button.disabled = true;
-
   document.addEventListener("click", function(event) {
-   
     var img = document.getElementById("exampleImage");
     if (img) {
       
@@ -61,33 +46,25 @@ button.addEventListener("click", function() {
       button.disabled = false;
     }
   });
-
-  
   button.addEventListener("click", function(event) {
     event.stopPropagation();
   });
 });
-
-
 // Calendarul si timp
 function updateDateTime() {
     const now = new Date();
     const days = ['Duminică', 'Luni', 'Marți', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă'];
     const months = ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'];
-    
     const dayName = days[now.getDay()];
     const day = now.getDate();
     const month = months[now.getMonth()];
     const year = now.getFullYear();
-    
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
-    
     document.getElementById('date').textContent = `${dayName}, ${day} ${month} ${year}`;
     document.getElementById('time').textContent = `${hours}:${minutes}:${seconds}`;
 }
-
 updateDateTime();
 setInterval(updateDateTime, 1000);
 
