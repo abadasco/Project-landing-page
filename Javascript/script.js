0e433d194ddfe12f635725b819e9526017301604
abadasco <arianbadasco@gmail.com>
2024-06-22 22:21:19 +0300

    bug fixes

--- Javascript/script.js
+++ Javascript/script.js
@@ -11,11 +11,9 @@ switchModeButton.addEventListener('click', () => {
 //Sidebar
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.querySelector(".main").style.marginLeft = "250px";
 }
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.querySelector(".main").style.marginLeft= "0";
 }
 //Timpul din main
 function updateClock() {
