0395ff2dfe609ba7bdec632adc756b4a9dd30f64
abadasco <arianbadasco@gmail.com>
2024-09-16 23:15:25 +0300

    File

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const switchModeButton = document.getElementById('switchMode');
 const themeLink = document.getElementById('theme');
 switchModeButton.addEventListener('click', () => {
-    if (themeLink.getAttribute('href') === '/css/light_mode.css') {
-        themeLink.setAttribute('href', '/css/dark_mode.css');
+    if (themeLink.getAttribute('href') === '/light_mode.css') {
+        themeLink.setAttribute('href', '/dark_mode.css');
     } else {
-        themeLink.setAttribute('href', '/css/light_mode.css');
+        themeLink.setAttribute('href', '/light_mode.css');
     }
 });
 function openNav() {
