8cfe7bc8424f865aa6ea95b5706941f7ba3112ca
abadasco <163906575+abadasco@users.noreply.github.com>
2024-10-18 18:56:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const switchModeButton = document.getElementById('switchMode');
 const themeLink = document.getElementById('theme');
 switchModeButton.addEventListener('click', () => {
-    if (themeLink.getAttribute('href') === '/dark_mode.css') {
-        themeLink.setAttribute('href', '/light_mode.css');
+    if (themeLink.getAttribute('href') === 'dark_mode.css') {
+        themeLink.setAttribute('href', 'light_mode.css');
     } else {
-        themeLink.setAttribute('href', '/dark_mode.css');
+        themeLink.setAttribute('href', 'dark_mode.css');
     }
 });
 function openNav() {
